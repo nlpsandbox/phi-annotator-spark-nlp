5153a18ffb325e77d230aed7c0a1d15efeaa7f25
tschaffter <thomas.schaffter@gmail.com>
2021-07-29 21:50:50 -0700

    Change port of Apache spark and tool

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$1" = 'uwsgi' ] || [ "$1" = 'python' ]; then
     cd ${APP_DIR}
-    # Start Apache Spark on port 8080
-    bash start-master.sh
+    # Start Apache Spark on port 8081
+    bash start-master.sh --webui-port 8081
     # Start the container command
     exec gosu nlp "$@"
 fi
