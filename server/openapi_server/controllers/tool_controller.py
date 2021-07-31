16650146a3a609034ea3f089620f28e40a180466
tschaffter <thomas.schaffter@gmail.com>
2021-07-31 10:02:46 -0700

    Bump project version to 0.1.1

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -14,7 +14,7 @@ def get_tool():  # noqa: E501
     """
     tool = Tool(
         name="phi-annotator-spark-nlp",
-        version="0.1.0",
+        version="0.1.1",
         license=License.APACHE_2_0,
         repository="github:nlpsandbox/phi-annotator-spark-nlp",
         description="Spark NLP-based PHI annotator",
